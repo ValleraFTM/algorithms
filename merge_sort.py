67d13048052435912abb51065a2a4506010d24ca
ValleraFTM <sagdacov@mail.ru>
2021-10-10 17:48:09 +0300

    merge_sort complite

--- merge_sort.py
+++ merge_sort.py
@@ -6,13 +6,35 @@ start_time = time.monotonic()
 
 A = [randint(0, 1000) for x in range(10000)]
 
-for i in range(0, len(A)):
-    smallest = i
-    for j in range(i + 1, len(A)-1):
-        if A[j] < A[smallest]:
-            smallest = j
-    A[i], A[smallest] = A[smallest], A[i]
+def merge_sort(A):
+    if len(A) == 1 or len(A) == 0:
+        return A
+    L, R = A[:len(A) // 2], A[len(A) // 2:]
+    merge_sort(L)
+    merge_sort(R) 
+    n = m = k = 0
+    C = [0] * (len(L) + len(R))
+    while n < len(L) and m < len(R):
+        if L[n] <= R[m]:
+            C[k] = L[n]
+            n += 1
+        else:
+            C[k] = R[m]
+            m += 1
+        k += 1
+    while n < len(L):
+        C[k] = L[n]
+        n += 1
+        k += 1
+    while m < len(R):
+        C[k] = R[m]
+        m += 1
+        k += 1
+    for i in range(len(A)):
+        A[i] = C[i]
+    return A
 
+merge_sort(A)
 
 end_time = time.monotonic()
 print(timedelta(seconds=end_time - start_time))

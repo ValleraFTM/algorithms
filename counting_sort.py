ee14b38ea0fe6ea7f55db3c6e37df44f1115f66b
ValleraFTM <sagdacov@mail.ru>
2021-10-26 20:42:17 +0300

    сортировка подсчётом завершена

--- counting_sort.py
+++ counting_sort.py
@@ -12,9 +12,10 @@ A = [randint(0, k) for x in range(200000)]
 
 def simple_counting_sort(A):
     C = [0 for i in range(0,k + 1)]
-    print(len(C))
+    
     for i in range(0, len(A)):
         C[A[i]] = C[A[i]] + 1
+    
     b = 0
     for j in range(0, k + 1):
         for i in range(0, C[j]):
@@ -22,22 +23,26 @@ def simple_counting_sort(A):
             b += 1
         
 def counting_sort(A):
-    C = [0 for i in range(0,k)]
+    C = [0 for i in range(0,k + 1)]
     
-    for j in range(0, len(A)):
+    for j in range(len(A)):
         C[A[j]] = C[A[j]] + 1
-    for i in range(0, k):
-        C[i] = C[i] + C[i-1]
+    C[0] = C[0] - 1
+    for i in range(1, k + 1):
+        C[i] = C[i] + C[i - 1]
 
-    B = [None for i in range(0, k)]
+
+    B = [None for i in range(len(A))]
     
-    for j in range (len(A)-1, -1, -1):
-        B[C[A[j]]] = A[j]
-        C[A[j]] = C[A[j]] - 1
+    for j in reversed(A):
+        B[C[j]] = j
+        C[j] = C[j] - 1
 
-simple_counting_sort(A)
-#counting_sort(A)
-print(A)
+#раскоментить для запуска простой сортировки подсчётом
+#simple_counting_sort(A)
+
+counting_sort(A)
+
 
 
 end_time = time.monotonic()
